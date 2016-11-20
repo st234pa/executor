f09dd19644c21ce70e5ca3d1eedec4721f9f2c29
st234pa <syoon1@stuy.edu>
2016-11-19 22:05:38 -0500

    finished

--- exec.c
+++ exec.c
@@ -3,30 +3,21 @@
 #include <unistd.h>
 #include <string.h>
 
-int main(){//int argc, char * argv[]){
+int main(){
   printf("what would you like to do? ");
   char command[100];
   fgets(command, sizeof(command), stdin);
   *strchr(command, '\n') = 0;
+  printf("%s", command);
   int i = 0;
   char* word[50];
-  char *a = " ";
   char *x = command;
-  while(command){
-    word[i] = strsep(&x, " ");
+  printf("%s", x);
+  while(word[i] = strsep(&x, " ")){
     i++;
   }
   i = 0;
-  while(i < 100){
-    printf("%s", word[i]);
-    i++;
-
-
-
-
-
-
-
-  }
+  execvp(word[0], word); 
+  return 0;
 }
  
